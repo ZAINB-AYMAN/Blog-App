a13f558fd22eb540f288e2c13f9fe7ccb8f9dd65
Zeinab Ayman <zainb.ayman.amer@gmail.com>
2026-03-05 18:06:17 +0200

    enviroment edit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Blog-App
 Node.js Course Assignments
+
+git clone https://github.com/ZAINB-AYMAN/Blog-App.git
+
+MONGO_URI=your_mongodb_url
+PORT=3001
+
+npm install
+
+npm start
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,10 @@
+import  dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config()
 
 export const databaseConntection = async ()=>{
-   const conntection = await mongoose.connect('mongodb://localhost:27017/blogApp').then(()=>{
+   const url = process.env.MONGO_URI
+   const conntection = await mongoose.connect(url).then(()=>{
         console.log("database conntected");
      }).catch ((err)=>{
         console.log(err);
